515c7e683740ec506212b412b79595055d1b6263
milesch <2miles4003@gmail.com>
2023-10-02 16:25:59 -0700

    added a function that merges the two busy schedules

--- project1_starter.cpp
+++ project1_starter.cpp
@@ -1,1 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
 
+int main() {
+
+}
+
+// merges busySchedule1 and busySchedule2: sorted by start time and replaces if there is any overlap
+vector<string> mergedSchedule(vector<string> schedule1, vector<string> schedule2) {
+    vector<string> merged;
+    int i = 0;
+    int j = 0;
+
+    while (i < schedule1.size() && j < schedule2.size()) {
+        if (schedule1[i] <= schedule2[j]) {
+            merged.push_back(schedule1[i]);
+            i++;
+        } else {
+            merged.push_back(schedule2[j]);
+            j++;
+        }
+    }
+
+    while (i < schedule1.size()) {
+        merged.push_back(schedule1[i]);
+        i++;
+    }
+
+    while (j < schedule2.size()) {
+        merged.push_back(schedule2[j]);
+        j++;
+    }
+
+    return merged;
+}
