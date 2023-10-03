63ee449a0bd943eb1b8b91a0c8a14be9e3c06086
nguyenkelly21 <112154106+nguyenkelly21@users.noreply.github.com>
2023-10-02 22:48:14 -0700

    Update project1_starter.cpp

--- project1_starter.cpp
+++ project1_starter.cpp
@@ -1,36 +1,129 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <sstream>
+#include <algorithm>
+#include <string>
+using namespace std;
 
-int main() {
-
+int hourToMinute(string times){
+  std::stringstream conversion(times);
+  int hour, minute;
+  char holder;
+  conversion >> hour >> holder >> minute;
+  int new_time = hour*60 + minute;
+  return new_time;
 }
 
-// merges busySchedule1 and busySchedule2: sorted by start time and replaces if there is any overlap
-vector<string> mergedSchedule(vector<string> schedule1, vector<string> schedule2) {
-    vector<string> merged;
-    int i = 0;
-    int j = 0;
+string minuteToString(int times){
+  string new_time;
+  int hour = times/60;
+  int minute = times%60;
+  string hour1 = to_string(hour);
+  string minute1 = to_string(minute);
+  if(minute < 10){
+      minute1 = "0" + minute1;
+  }
+  new_time = hour1 + ":" + minute1;
+  return new_time;
+}
 
-    while (i < schedule1.size() && j < schedule2.size()) {
-        if (schedule1[i] <= schedule2[j]) {
-            merged.push_back(schedule1[i]);
+vector<string> stringToVector(string schedule){
+    vector<string> v;
+    istringstream iss(schedule);
+    string temp;
+    while (std::getline(iss, temp, ',')) {
+        v.push_back(temp);
+    }
+    return v;
+}
+
+string remove_character(string schedule){
+    int i = 0;
+    while(i < schedule.size()) {
+        if(schedule[i] == '[' || schedule[i] == ']' || schedule[i] == '\'') {
+            schedule.erase(i,1);
+        } else{
             i++;
-        } else {
-            merged.push_back(schedule2[j]);
-            j++;
         }
     }
+    return schedule;
+}
 
-    while (i < schedule1.size()) {
-        merged.push_back(schedule1[i]);
-        i++;
+int main() {
+  ifstream file("input.txt");
+  ofstream output("output.txt");
+  string person1_busy_Schedule, person1_work_hours, person2_busy_Schedule, person2_work_hours;
+  int duration_of_meeting;
+  while(!file.eof()) {
+    file >> person1_busy_Schedule >> person1_work_hours >> person2_busy_Schedule >> person2_work_hours >> duration_of_meeting;
+    person1_busy_Schedule = remove_character(person1_busy_Schedule);
+    person1_work_hours = remove_character(person1_work_hours);
+    person2_busy_Schedule = remove_character(person2_busy_Schedule);
+    person2_work_hours = remove_character(person2_work_hours);
+
+    vector<string> person1 = stringToVector(person1_busy_Schedule);
+    vector<string> person2 = stringToVector(person2_busy_Schedule);
+    vector<string> hour1 = stringToVector(person1_work_hours);
+    vector<string> hour2 = stringToVector(person2_work_hours);
+      
+    int sizes = person1.size() + person2.size();
+    for(int i = 0; i < person1.size(); i++){
+      person2.push_back(person1[i]);
     }
 
-    while (j < schedule2.size()) {
-        merged.push_back(schedule2[j]);
-        j++;
+    vector<int> minutes;
+    for(int i = 0; i<person2.size(); i++){
+      minutes.push_back(hourToMinute(person2[i]));
     }
+    
+    int colum = minutes.size()/2;
+    vector<vector<int>> combined(colum, vector<int>(2));
+    int index1 = 0;
+    for(int row = 0; row < combined.size(); ++row){
+      for(int col = 0; col < combined[row].size(); ++col){
+        if(index1 < minutes.size()){
+          combined[row][col]= minutes[index1];
+          index1++;
+        }
+      }
+    }
+    sort(combined.begin(), combined.end());
+    vector<int> new_hour; //holds least and most time free
+    if(hour1[0] > hour2[0]){
+        new_hour.push_back(hourToMinute(hour1[0]));
+    } else{
+        new_hour.push_back(hourToMinute(hour2[0]));
+    }
+    if(hour1[1] < hour2[1]){
+      new_hour.push_back(hourToMinute(hour1[1]));
+    } else{
+      new_hour.push_back(hourToMinute(hour2[1]));
+    }
+    int start = new_hour[0];
+    int end = new_hour[1];
 
-    return merged;
+    std::vector<std::pair<string, string>> free_time;
+    
+    for (const auto& a : combined) {
+        while((start + duration_of_meeting) <= a[0]){
+          free_time.push_back({minuteToString(start), minuteToString(start+duration_of_meeting)});
+          start = start + duration_of_meeting;
+        } 
+        start = a[1];
+    }
+    
+    while((start + duration_of_meeting) <= end){
+        free_time.push_back({minuteToString(start), minuteToString(start+duration_of_meeting)});
+        start = start + duration_of_meeting;
+    } 
+
+    
+    output <<"[";
+    for(int i = 0; i < free_time.size(); i++){
+        output << "[" <<free_time[i].first<<","<<free_time[i].second<<"], ";
+    }
+    output <<"]" << endl;
+  }
+  return 0;
 }
